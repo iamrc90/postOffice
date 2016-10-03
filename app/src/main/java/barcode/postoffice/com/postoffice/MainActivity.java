079e414f664251330ff30ad12f310eca436dfbd8
Ripul Chhabra <ripulchhabra@Ripuls-MacBook-Pro.local>
2016-10-03 23:31:47 +0530

    initial commit

---
+++ app/src/main/java/barcode/postoffice/com/postoffice/MainActivity.java
@@ -0,0 +1,34 @@
+package barcode.postoffice.com.postoffice;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebview ;
+    private String URL = "http://54.69.174.4/html/index.php";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        mWebview  = new WebView(this);
+
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+
+        final Activity activity = this;
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mWebview .loadUrl(URL);
+        setContentView(mWebview );
+    }
+}
